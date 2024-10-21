c4b15ccfadce34dbd8f846b9348af973c2d3b4ca
jaiden-lee <jaidenwlee@gmail.com>
2024-10-21 16:58:38 -0400

    More initial set up + Navbar + Home page url route

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods, require_GET
+
+# Create your views here.
+@require_GET
+def home_page(request):
+    return render(request, "main/home.html", {})
