3dd9bb80209c206d11aa4609dbb64fbe59135bfb
weswes2EPYC <whu327@gatech.edu>
2024-10-28 23:13:29 -0400

    finished with the top 3 songs formatting

--- main/views.py
+++ main/views.py
@@ -11,18 +11,43 @@ def spotify_wrapped_view(request):
     # Dummy Spotify data
     spotify_data = {
         'username': 'eskedie',
+        'slide1_background': static('Slide1.jpg'),
+        'slide2_background': static('Slide2.jpg'),
+        'slide4_background': static('Slide4.jpg'),
+        'slide6_background': static('Slide6.jpg'),
+        'slide8_background': static('Slide8.jpg'),
         'tracks': [
             {
                 'name': 'Track One',
                 'artist': 'Artist One',
-                'album_cover': static('Slide1.jpg')
+                'album_cover': 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228'
             },
             {
                 'name': 'Track Two',
                 'artist': 'Artist Two',
-                'album_cover': 'https://via.placeholder.com/600x800.png?text=Album+Cover+2'
+                'album_cover': 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228'
+            },
+            {
+                'name': 'Track Three',
+                'artist': 'Artist Three',
+                'album_cover': 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228'
             }
-        ]
+        ],
+        'artists': [
+            {
+                'name': 'KSI',
+                'artist_image': 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228'
+            },
+            {
+                'name': 'PlayBoi Carti',
+                'artist_image': 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228'
+            },
+            {
+                'name': 'Red Hot Chili Peppers',
+                'artist_image': 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228'
+            }
+        ],
+        'summary': 'lorem ipsium or something idk the rest'
     }
     
     return render(request, 'main/slideshow.html', {'spotify_data': spotify_data})
