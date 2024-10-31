220cff4f55995df3c7cbf9289635b25630b51926
jaydenwen25 <jayden.wen@gmail.com>
2024-10-31 12:57:47 -0400

    wraps page
    working favorites button

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from django.views.decorators.http import require_http_methods, require_GET
 # Create your views here.
 @require_GET
 def home_page(request):
-    return render(request, "main/home.html", {})
+    return render(request, "main/my_wraps.html", {})
