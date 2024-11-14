8b4130de1af7a63887f95077ac012b51cb86315f
sherryliprogramming <sherryli.gtcs@gmail.com>
2024-11-14 15:01:25 -0500

    create page

--- main/views.py
+++ main/views.py
@@ -5,3 +5,6 @@ from django.views.decorators.http import require_http_methods, require_GET
 @require_GET
 def home_page(request):
     return render(request, "main/home.html", {})
+
+def myWraps(request):
+    return render(request, "main/mywraps.html", {})
