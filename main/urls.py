d3bab1511bae0b97e3db0598577cc495edf35d5b
weswes2EPYC <whu327@gatech.edu>
2024-11-15 11:24:32 -0500

    changed scaling for slideshow

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page),
-    path('mywraps', views.myWraps)
+    path('mywraps', views.myWraps),
     path('wrapped/', views.spotify_wrapped_view, name='spotify_wrapped'),
 ]
